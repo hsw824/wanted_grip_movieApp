0757302cc9a90afdb89ed52a5f4d2d2b1d971d3f
hsw824 <ghdtjddn0824@naver.com>
2022-05-11 16:03:17 +0900

    Design: Movie list and overall file design

--- src/routes/Search/MovieList/index.tsx
+++ src/routes/Search/MovieList/index.tsx
@@ -1,25 +1,26 @@
-import { ProductProps } from '../../../types/ProductList'
+import { ProductProps } from "../../../types/ProductList";
+import styles from "./movieList.module.scss";
 
 const MovieList = ({ data }: { data: ProductProps[] }) => {
   if (!data) {
-    return null
+    return <h1>검색 결과가 없습니다</h1>;
   }
   return (
-    <div>
+    <div className={styles.movieList}>
       <ul>
         {data.map((item, index) => (
-          <div key={`movie-key-${index}`}>
-            <li>
-              <img src={item.Poster} alt='movie poster' />
+          <div className={styles.movieCard} key={`movie-key-${index}`}>
+            <li className={styles.poster}>
+              <img src={item.Poster} alt="movie poster" />
             </li>
-            <li>{item.Title}</li>
-            <li>{item.Year}</li>
-            <li>{item.Type}</li>
+            <li className={styles.title}>{item.Title}</li>
+            <li className={styles.year}>{item.Year}</li>
+            <li className={styles.type}>{item.Type}</li>
           </div>
         ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export { MovieList }
+export { MovieList };
--- src/routes/Search/index.tsx
+++ src/routes/Search/index.tsx
@@ -1,43 +1,48 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import styles from './Search.module.scss'
-import axios from 'axios'
-import { ProductProps } from '../../types/ProductList'
-import { MovieList } from './MovieList'
+import styles from "./search.module.scss";
+import axios from "axios";
+import { ProductProps } from "../../types/ProductList";
+import { MovieList } from "./MovieList";
 
 const Search = () => {
-  const [value, setValue] = useState<string>('')
-  const [data, setData] = useState<ProductProps[]>([])
-  const [loading, setLoading] = useState<boolean>(true)
+  const [value, setValue] = useState<string>("");
+  const [data, setData] = useState<ProductProps[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
 
-  const MOVIE_BASE_URL = `http://www.omdbapi.com/?apikey=92e32667&s=${value}&page={페이지번호(2)}`
+  const MOVIE_BASE_URL = `http://www.omdbapi.com/?apikey=92e32667&s=${value}&page={페이지번호(1)}`;
 
   const fetchData = async () => {
-    const response = await axios.get(`${MOVIE_BASE_URL}`)
-    setData(response.data.Search)
-  }
+    const response = await axios.get(`${MOVIE_BASE_URL}`);
+    setData(response.data.Search);
+  };
 
   const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setValue(e.currentTarget.value)
-  }
+    setValue(e.currentTarget.value);
+  };
 
   const handleOnSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault()
-    fetchData()
-    setLoading(false)
-    setValue('')
-  }
+    e.preventDefault();
+    fetchData();
+    setLoading(false);
+    setValue("");
+  };
   return (
     <div className={styles.search}>
       <form>
-        <input type='text' placeholder='영화를 검색해 주세요' value={value} onChange={handleOnChange} />
-        <button type='submit' onClick={handleOnSubmit}>
-          검색하기
+        <input
+          type="text"
+          placeholder="Search for a movie"
+          value={value}
+          onChange={handleOnChange}
+        />
+        <button type="submit" onClick={handleOnSubmit}>
+          Search
         </button>
       </form>
-      {loading ? <h1>검색 결과가 없습니다</h1> : <MovieList data={data} />}
+      {loading ? <h1>No search results found</h1> : <MovieList data={data} />}
     </div>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
