dbbd9037dcffb72fce1848e5fb1e2fb7fa5a80ce
hsw824 <ghdtjddn0824@naver.com>
2022-05-15 10:28:26 +0900

    Feat: Add route

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,11 +1,18 @@
 import styles from './Routes.module.scss'
 import Search from './Search'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Favorite from './Favorite'
 
 const App = () => {
   return (
-    <div className={styles.app}>
-      <Search />
-    </div>
+    <BrowserRouter>
+      <div className={styles.app}>
+        <Routes>
+          <Route path='/' element={<Search />} />
+          <Route path='/favorite' element={<Favorite />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
 
